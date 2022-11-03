048d3cb82aec03aee9fb7b5502a418c0013d8fa5
TheLonelyDude <magnus.kongstad@gmail.com>
2022-11-03 09:56:12 +0100

    Updoot

--- Streamingtjeneste/src/Main.java
+++ Streamingtjeneste/src/Main.java
@@ -1,6 +1,17 @@
+import genre.IGenre;
+import genre.MovieGenre;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<IGenre> genres = new ArrayList<>(new ArrayList<>(List.of(MovieGenre.values())));
+
+        for (IGenre genre : genres) {
+                System.out.print(genre);
+        }
 
     }
 
