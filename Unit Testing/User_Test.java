b18b02a17c88e481f544b3a026514de4385a8ccd
TheLonelyDude <magnus.kongstad@gmail.com>
2022-11-07 10:31:59 +0100

    Fixed unit testing

--- Unit Testing/User_Test.java
+++ Unit Testing/User_Test.java
@@ -1,14 +1,22 @@
 import org.junit.jupiter.api.Test;
 import user.IUser;
+import user.User;
 import utils.data.FileIO;
 
 import java.util.ArrayList;
 
-public class User_Test {
+import static org.junit.jupiter.api.Assertions.*;
+
+class User_Test {
     @Test
-    public void testNewUser() {
+    void testNewUser() {
         FileIO fileIO = new FileIO();
         ArrayList<IUser> users = fileIO.loadUsers();
+        IUser user = new User(-1, "user1", "testmail", "password1", 21, new ArrayList<>(), new ArrayList<>());
+
+        user.save(users);
+
+        assertEquals("user1", user.getName());
     }
 
 }
