1ef4b1002f7b352f97465d64b8155e323e9b1d88
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-11-16 15:09:53 +0100

    new constructor and added set and add to all arraylists

--- Streamingtjeneste/src/main/utils/data/SessionCache.java
+++ Streamingtjeneste/src/main/utils/data/SessionCache.java
@@ -12,10 +12,49 @@ public class SessionCache {
     private final ArrayList<ISeries> series;
     private final ArrayList<IUser> users;
 
-    public SessionCache(ArrayList<IMovie> movie, ArrayList<ISeries> series, ArrayList<IUser> users) {
-        this.movies = movie;
-        this.series = series;
-        this.users = users;
+    public SessionCache() {
+        this.movies = new ArrayList<>();
+        this.series = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    public void setMovies(ArrayList<IMovie> movies) {
+        this.movies.clear();
+        this.movies.addAll(movies);
+    }
+
+    public void setSeries(ArrayList<ISeries> series) {
+        this.series.clear();
+        this.series.addAll(series);
+    }
+
+    public void setUsers(ArrayList<IUser> users) {
+        this.users.clear();
+        this.users.addAll(users);
+    }
+
+    public boolean addMovie(IMovie movie) {
+        if (!this.movies.contains(movie)) {
+            this.movies.add(movie);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addSeries(ISeries series) {
+        if (!this.series.contains(series)) {
+            this.series.add(series);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addUser(IUser user) {
+        if (!this.users.contains(user)) {
+            this.users.add(user);
+            return true;
+        }
+        return false;
     }
 
     public ArrayList<IMovie> getMovies() {
