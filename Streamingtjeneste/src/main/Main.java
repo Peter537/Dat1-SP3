34283bf96ea50ecd6bd8c6616320aeb8e5dddabe
TheLonelyDude <magnus.kongstad@gmail.com>
2022-11-16 12:36:31 +0100

    DB connection made to schema "chillmedia""

--- Streamingtjeneste/src/main/Main.java
+++ Streamingtjeneste/src/main/Main.java
@@ -1,8 +1,12 @@
 package main;
 
+import main.utils.data.DataBaseIO;
+
 public class Main {
 
     public static void main(String[] args) {
+        DataBaseIO dbio = new DataBaseIO();
+
         ChillMedia cm = new ChillMedia();
         cm.run();
     }
--- Streamingtjeneste/src/main/utils/data/DataBaseIO.java
+++ Streamingtjeneste/src/main/utils/data/DataBaseIO.java
@@ -5,14 +5,16 @@ import main.media.ISeries;
 import main.user.IUser;
 import main.utils.data.dbutil.MySQL;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DataBaseIO implements IDataIO {
     MySQL mySQL;
 
     public DataBaseIO() {
         mySQL = new MySQL();
-        mySQL.openConnection("localhost", "ChillMedia", "root", "12321");
+        mySQL.openConnection("localhost", "chillmedia", "root", getPassword());
     }
 
     public ArrayList<IUser> loadUsers() {
@@ -34,7 +36,14 @@ public class DataBaseIO implements IDataIO {
     private String getPassword() {
         String password = "";
 
+        File file = new File("Data/dbpass.csv");
 
+        try {
+            Scanner scanner = new Scanner(file);
+            password = scanner.nextLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return password;
     }
