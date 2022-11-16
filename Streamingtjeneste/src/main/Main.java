11b375f8fd74361f032d7a0b1adb04f3b858a702
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-11-16 15:08:41 +0100

    removed unused code

--- Streamingtjeneste/src/main/Main.java
+++ Streamingtjeneste/src/main/Main.java
@@ -1,12 +1,8 @@
 package main;
 
-import main.utils.data.DataBaseIO;
-
 public class Main {
 
     public static void main(String[] args) {
-        DataBaseIO dbio = new DataBaseIO();
-
         ChillMedia cm = new ChillMedia();
         cm.run();
     }
