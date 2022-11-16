cb17d29c124b1969b28f52297b814b6b65a2b444
TheLonelyDude <magnus.kongstad@gmail.com>
2022-11-16 12:10:14 +0100

    Added DB IO

---
+++ Streamingtjeneste/src/main/utils/data/DataBaseIO.java
@@ -0,0 +1,25 @@
+package main.utils.data;
+
+import main.media.IMovie;
+import main.media.ISeries;
+import main.user.IUser;
+
+import java.util.ArrayList;
+
+public class DataBaseIO implements IDataIO{
+    public ArrayList<IUser> loadUsers() {
+        return null;
+    }
+
+    public ArrayList<IMovie> loadMovies() {
+        return null;
+    }
+
+    public ArrayList<ISeries> loadSeries() {
+        return null;
+    }
+
+    public void save(ArrayList<IUser> users) {
+
+    }
+}
