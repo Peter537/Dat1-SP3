8403f131b4e40fc962b27853347f50d18b5fdc73
Skainsmate <113088906+Skainsmate@users.noreply.github.com>
2022-11-17 10:41:04 +0100

    SQL statements for searching in movies and series created

--- Streamingtjeneste/src/main/utils/data/dbutil/SQLStatements.java
+++ Streamingtjeneste/src/main/utils/data/dbutil/SQLStatements.java
@@ -16,4 +16,45 @@ public class SQLStatements {
     public static String getUser(String username) {
         return "SELECT * FROM chillmedia.user WHERE username = '" + username + "'";
     }
+    public static String getUserFromEmail(String email)
+    {
+        return  "SELECT * FROM chillmedia.user WHERE email = '" + email + "'";
+    }
+
+    public static String getMovieFromName (String name)
+    {
+        return "SELECT * FROM chillmedia.movie WHERE name LIKE '" + name +"%'";
+    }
+
+    public static String getMovieFromGenre (String genre)
+    {
+        return "SELECT * FROM chillmedia.movie WHERE genres = '" + genre +"'";
+    }
+
+    public static String getMovieWithRating (String rating)
+    {
+        return "SELECT * FROM chillmedia.movie WHERE rating >= '" + rating +"'";
+    }
+
+
+
+
+public static String getSeriesFromName (String name)
+{
+    return "SELECT * FROM chillmedia.series WHERE name LIKE '" + name +"%'";
+}
+
+public static String getSeriesFromGenre (String genre)
+{
+    return "SELECT * FROM chillmedia.series WHERE genres = '" + genre +"'";
+}
+
+public static String getSeriesWithRating (String rating)
+{
+    return "SELECT * FROM chillmedia.series WHERE rating >= '" + rating +"'";
+}
+
+
+
+
 }
