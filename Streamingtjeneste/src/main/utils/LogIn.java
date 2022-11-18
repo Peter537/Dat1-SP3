a7a4c7f147928825cf0fa1d0ac6a4c9db71625ac
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-11-18 11:44:46 +0100

    Update LogIn.java

--- Streamingtjeneste/src/main/utils/LogIn.java
+++ Streamingtjeneste/src/main/utils/LogIn.java
@@ -140,7 +140,7 @@ public class LogIn {
             }
         }
         this.currentUser = user;
-        chillMedia.getDataIO().setCache(user);
+        getChillMedia().getDataIO().setCache(user);
     }
 
     /**
@@ -218,7 +218,7 @@ public class LogIn {
         return currentUser;
     }
 
-    public ChillMedia getChillMedia() {
+    private ChillMedia getChillMedia() {
         return chillMedia;
     }
 }
