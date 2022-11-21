0a5f6636fae61b9dbdfda73313d504aa25e3d79b
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-11-21 14:38:39 +0100

    removed SQL statements we dont use

--- Streamingtjeneste/src/main/utils/data/dbutil/SQLStatements.java
+++ Streamingtjeneste/src/main/utils/data/dbutil/SQLStatements.java
@@ -24,28 +24,6 @@ public class SQLStatements {
     }
 
     /**
-     * NOT IN USE, NOT IN UML
-     *
-     * returns the user queried by username
-     *
-     * @param username
-     * @return String
-     */
-    public static String getUserFromUserName(String username) {
-        return "SELECT * FROM chillmedia.user WHERE username = '" + username + "'";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param email
-     * @return String
-     */
-    public static String getUserFromEmail(String email) {
-        return  "SELECT * FROM chillmedia.user WHERE email = '" + email + "'";
-    }
-
-    /**
      * queries the SQL database for a user by email and passowrd
      *
      * @param email the inputted email
@@ -65,75 +43,5 @@ public class SQLStatements {
      */
     public static String getMoviesFromUserByEmailAndPassword(String email, String password) {
         return  "SELECT u.user_id, um.um_user_id, um.um_movie_id, um.um_movie_status FROM user_movie AS um JOIN user AS u ON u.user_id = um.um_user_id WHERE u.email = '"+ email +"' AND u.password = '" + password + "' AND u.user_id = um_user_id";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param id
-     * @return String
-     */
-    public static String getUserFromID(int id) {
-        return  "SELECT * FROM chillmedia.user WHERE user_id = '" + id + "'";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param name
-     * @return String
-     */
-    public static String getMovieFromName(String name) {
-        return "SELECT * FROM chillmedia.movie WHERE name LIKE %'" + name +"%'";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param genre
-     * @return String
-     */
-    public static String getMovieFromGenre(String genre) {
-        return "SELECT * FROM chillmedia.movie WHERE genres LIKE = %'" + genre +"'%";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param minimumRating
-     * @return String
-     */
-    public static String getMovieWithRating(String minimumRating) {
-        return "SELECT * FROM chillmedia.movie WHERE rating >= '" + minimumRating +"'";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param name
-     * @return String
-     */
-    public static String getSeriesFromName(String name) {
-        return "SELECT * FROM chillmedia.series WHERE name LIKE %'" + name +"%'";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param genre
-     * @return String
-     */
-    public static String getSeriesFromGenre(String genre) {
-        return "SELECT * FROM chillmedia.series WHERE genres LIKE= %'" + genre +"%'";
-    }
-
-    /**
-     * NOT IN USE, NOT IN UML
-     *
-     * @param minimumRating
-     * @return String
-     */
-    public static String getSeriesWithRating(String minimumRating) {
-        return "SELECT * FROM chillmedia.series WHERE rating >= '" + minimumRating +"'";
     }
 }
