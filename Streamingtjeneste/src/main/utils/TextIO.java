b1b9f19ba7a22d6c660038af3e0173c0253caff8
Yusuf Al-Khafaji <113047451+StylizedAce@users.noreply.github.com>
2022-11-21 13:45:57 +0100

    added TextIO comments

--- Streamingtjeneste/src/main/utils/TextIO.java
+++ Streamingtjeneste/src/main/utils/TextIO.java
@@ -10,10 +10,10 @@ public class TextIO {
     private static final Scanner scanner = new Scanner(System.in);
 
     /**
+     * This method displays a message to the user and then prompts them for a response
      *
-     *
-     * @param msg
-     * @return String
+     * @param msg is the message the user will be shown
+     * @return String is the string value the user inputs afterwards
      */
     public static String getUserInput(String msg){
         print(msg);
@@ -21,11 +21,12 @@ public class TextIO {
     }
 
     /**
+     * This method is an overload that not only displays a message and lets the user response, but it also shows a
+     * list of options before the user responds
      *
-     *
-     * @param msg
-     * @param options
-     * @return String
+     * @param msg is the message the user will be shown
+     * @param options is the list of options the user can choose from
+     * @return String is the string value the user has inputted
      */
     public static String getUserInput(String msg, String[] options) {
         println(msg);
@@ -36,12 +37,13 @@ public class TextIO {
     }
 
     /**
+     * This method is similar to the above message in letting the user choose from a displayed list of options.
+     * The difference is that this method has a startindex as index '0' is reserved for the "go back" function
      *
-     *
-     * @param msg
-     * @param startIndex
-     * @param options
-     * @return String
+     * @param msg is the message the user will be shown
+     * @param startIndex Is the startindex for the options (usually starts at 1, as 0 is reserved for the "go back" function)
+     * @param options is the list of options the user can choose from
+     * @return String is the string value the user has inputted
      */
     public static String getUserInput(String msg, int startIndex, String[] options) {
         println(msg);
@@ -52,13 +54,14 @@ public class TextIO {
     }
 
     /**
+     * This is a helper method that displays the page to the user and shows the user how to navigate the page of
+     * media the user queried for. It then also lets the user choose from the displayed media based on the query
      *
-     *
-     * @param msg
-     * @param page
-     * @param pageSize
-     * @param media
-     * @return String
+     * @param msg is a message the user will be shown
+     * @param page is the page number that the user is on
+     * @param pageSize is the page size (here we have chosen it should be 10 movies shown per page)
+     * @param media is the list of media that was loaded onto that page
+     * @return String is the user's input for their choice of media
      */
     public static String getUserInputFromMedia(String msg, int page, int pageSize, ArrayList<IMedia> media) {
         println(msg);
@@ -72,18 +75,18 @@ public class TextIO {
     }
 
     /**
+     * We made a method for println so we can rid the program of cluttering "System.out.println"-lines
      *
-     *
-     * @param msg
+     * @param msg is the message to be displayed
      */
     public static void println(String msg) {
         System.out.println(msg);
     }
 
     /**
+     * We made a method for println so we can rid the program of cluttering "System.out.print"-lines
      *
-     *
-     * @param msg
+     * @param msg is the message to be displayed
      */
     public static void print(String msg) {
         System.out.print(msg);
